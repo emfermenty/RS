0cbc78b38dc9f1fc36dc52da535fb719f2239863
emfermenty <demrar12345@gmail.com>
2025-04-25 12:46:17 +0500

    lab71

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Пример запуска: 5000 127.0.0.1 5001 true
+        if (args.Length < 3)
+        {
+            Console.WriteLine("Использование: <myPort> <nextIp> <nextPort> [true/false для токена]");
+            return;
+        }
+
+        int myPort = int.Parse(args[0]);
+        string nextIp = args[1];
+        int nextPort = int.Parse(args[2]);
+        bool startWithToken = args.Length > 3 && bool.Parse(args[3]);
+
+        var node = new TokenRingNode(myPort, nextIp, nextPort, startWithToken);
+        node.Start();
+    }
+}
--- TokenRingNode.cs
+++ TokenRingNode.cs
@@ -1,12 +1,91 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
 
-namespace TokenRing
+class TokenRingNode
 {
-    internal class TokenRingNode
+    private readonly int myPort;
+    private readonly string nextIp;
+    private readonly int nextPort;
+    private readonly bool isInitialHolder;
+
+    private UdpClient udpClient;
+    private bool running = true;
+
+    public TokenRingNode(int myPort, string nextIp, int nextPort, bool isInitialHolder = false)
     {
+        this.myPort = myPort;
+        this.nextIp = nextIp;
+        this.nextPort = nextPort;
+        this.isInitialHolder = isInitialHolder;
+    }
+
+    public void Start()
+    {
+        udpClient = new UdpClient(myPort);
+        Log($"Узел запущен на порту {myPort}. Следующий узел: {nextIp}:{nextPort}");
+
+        if (isInitialHolder)
+        {
+            Thread.Sleep(1000); // Ждём запуска других
+            Log("Узел стартует с токеном.");
+            SendToken();
+        }
+
+        while (running)
+        {
+            try
+            {
+                IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+                byte[] data = udpClient.Receive(ref remoteEP);
+                string message = Encoding.UTF8.GetString(data);
+
+                if (message == "TOKEN")
+                {
+                    Log($"Токен получен от {remoteEP}. Выполняется работа...");
+                    Thread.Sleep(500); // Эмуляция работы
+                    SendToken();
+                }
+            }
+            catch (SocketException se)
+            {
+                Log($"Сокет закрыт: {se.Message}");
+            }
+            catch (Exception ex)
+            {
+                Log($"Ошибка: {ex.Message}");
+            }
+        }
+    }
+
+    private void SendToken()
+    {
+        try
+        {
+            IPEndPoint nextEP = new IPEndPoint(IPAddress.Parse(nextIp), nextPort);
+            byte[] tokenData = Encoding.UTF8.GetBytes("TOKEN");
+            udpClient.Send(tokenData, tokenData.Length, nextEP);
+            Log($"Токен передан узлу {nextIp}:{nextPort}.");
+        }
+        catch (Exception ex)
+        {
+            Log($"Ошибка при отправке токена: {ex.Message}");
+        }
+    }
+
+    public void Stop()
+    {
+        running = false;
+        udpClient?.Close();
+    }
+
+    private void Log(string message)
+    {
+        string logLine = $"[{DateTime.Now:HH:mm:ss}] {message}";
+        Console.WriteLine(logLine);
+        File.AppendAllText($"log_{myPort}.txt", logLine + Environment.NewLine);
     }
 }
